cff4bd59ad297b8be264f248e47cf2ae11a47281
Rodpach <rodpacheco@hotmail.com>
2022-08-21 20:23:37 -0500

    update 21 aug.

--- invasion_process.R
+++ invasion_process.R
@@ -1,65 +1,70 @@
 invasion_process  = function(layer, date_col, poligon_id_col, max_date_ymd = NA){
   
-  require(dplyr)
-  require(sf)
-  require(lubridate)
-  require(tibble)
+  library(dplyr)
+  library(sf)
+  library(lubridate)
   
   
   if(is.na(max_date_ymd)){
-    max_date_ymd <- max(dplyr::pull(layer,"first_invasion"), na.rm = T)
-  } else{max_date_ymd = lubridate::ymd(max_date_ymd)}
+    max_date_ymd <- max(pull(layer,"first_invasion"), na.rm = T)
+  } else{max_date_ymd = ymd(max_date_ymd)}
   
   
   
   layer_int <- st_intersects(layer)
   
   
-  df =  lapply(as.list(1:dim(layer)[1]), function(x){
+  df <-  lapply(as.list(1:dim(layer)[1]), function(x){ #Obtain invasion data from each polygon and its adjacent polygons. returns a list of tibbles, one for each polygon.
     
-    plot_adj <- layer[layer_int[[x]],] #Seleccionamos los municips adjacentes
+    plot_adj <- layer[layer_int[[x]],] #Selects adjacent polygons.
     
-    obj <- layer[x,] #Seleccionamos el poligono objetivo
+    obj <- layer[x,] #Select focal polygon
     
-    fecha_1inv <- dplyr::pull(obj, date_col) #Seleccionamos la fecha de primera invasion en el municipio objetivo
+    fecha_1inv <- pull(obj, date_col) #Select the first date of invasion in the focal polygon
     
-    if(!is.na(fecha_1inv)){
-      neigh_prev <- dplyr::pull(dplyr::filter(plot_adj, get(date_col) < fecha_1inv), date_col)
-      neigh_num <- length(neigh_prev)
+    if(!is.na(fecha_1inv)){ #If the focal polygon is invaded
+      neigh_prev <- filter(plot_adj, get(date_col) < fecha_1inv) %>% #obtain the date of the first invaded adjacent cell 
+        pull(date_col)
       
-      if(neigh_num != 0 ){
+      neigh_num <- length(neigh_prev) #number of adjacent with invaded polygons
+      
+      if(neigh_num != 0 ){ #if the number of adjacent invaded polygons is not zero, it calculates de interval in months of 
         neigh_1inv <- interval(min(neigh_prev), fecha_1inv) %/% months(1)
-      } else{
-        neigh_1inv <- 0
+      } else{ #if the number of adjacent invaded polygons is zero, then it is 0.
+        neigh_1inv <- 0 
       }
       
-      invasion <- 1
-    } else{
-      invasion <- 0
+      invasion <- 1 #polygon defined as invaded
+    } else{ #If the focal polygon is not invaded
+      invasion <- 0 #polygon defined as not invaded
       
-      inv_0 <- layer[-x,]
+      inv_0 <- layer[subset(layer_int[[x]], layer_int[[x]] != x),] #Obtain adjacent polygons form focal polygon.
       
-      inv_0 <- layer[subset(layer_int[[x]], layer_int[[x]] != x),]
+      neigh_prev <- na.omit(pull(inv_0, date_col)) #Obtain invaded adjacent polygons
+      neigh_num <- length(neigh_prev) #Obtain number of invaded polygons.
       
-      neigh_prev <- na.omit(dplyr::pull(inv_0, date_col))
-      neigh_num <- length(neigh_prev)
-      
-      if(neigh_num != 0){
-        neigh_1inv <- interval(min(na.omit(dplyr::pull(plot_adj, date_col))), max_date_ymd) %/% months(1)
-      } else{
+      if(neigh_num != 0){  #if the number of adjacent invaded polygons is not zero, it calculates de interval in months of 
+        neigh_1inv <- interval(min(na.omit(pull(plot_adj, date_col))), max_date_ymd) %/% months(1) 
+      } else{ #if the number of adjacent invaded polygons is zero, then it is 0.
         neigh_1inv <- 0
       }
     }
     
-    tibble::tibble(poligon_id_col = pull(obj, poligon_id_col), period_months_first_invasion = neigh_1inv, invaded_neighbors = neigh_num, invaded = invasion)
+    #return obtained data for each polygon.
+    tibble(poligon_id_col = pull(obj, poligon_id_col), 
+           period_months_first_invasion = neigh_1inv, 
+           invaded_neighbors = neigh_num, 
+           invaded = invasion)
   })
   
-  df <- dplyr::bind_rows(df)
+  #concatenate the list of obtained data from each polygon.
+  df <- bind_rows(df)
   
+  #Set column names as their ids
   colnames(df)[1] <- poligon_id_col
   
-  
-  df <- suppressMessages(dplyr::left_join(layer, df))
+  #Join layer data with their invasion process.
+  df <- suppressMessages(left_join(layer, df))
   
   return(df)
 }
